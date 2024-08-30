b6bd26bd465a59b4f6d3b80837c1ef9c53cfe798
Sandra Fernandes <70379731+sendrastisch@users.noreply.github.com>
2024-08-30 22:48:40 +0200

    add routing to pdp

--- src/components/pokeItem/pokemonItem.jsx
+++ src/components/pokeItem/pokemonItem.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './pokemonItem.css';
 import pokeball from '../../assets/images/pokeball.png';
 
-const PokemonItem = ({id, name, image, type}) => {
+const PokemonItem = ({ id, name, image, type }) => {
 
     function capitalizeFirstLetter(word) {
         return word.charAt(0).toUpperCase() + word.slice(1);
@@ -54,27 +55,29 @@ const PokemonItem = ({id, name, image, type}) => {
     const backgroundColor = getBackgroundColor(type[0]);
 
     return (
-        <div className={'container-item'} style={{ backgroundColor }}>
-            <div className={`image-div`}>
-                <img className={'pokemon-img'} src={image} alt={name}/>
-                <img className={'pokeball-img'} src={pokeball} alt={"pokeball"}/>
-            </div>
-            <div className="name-id-div">
-                <p>#{id}</p>
-                <p className={`name`}>{capitalizeFirstLetter(name)}</p>
-                <div className={'type-div'}>
-                    {type.map((type, index) => (
-                        <p
-                            key={index}
-                            style={{backgroundColor: getBackgroundColor(type)}}
-                            className={`type-p`}
-                        >
-                            {capitalizeFirstLetter(type)}
-                        </p>
-                    ))}
+        <Link to={`/pokemon/${id}`} className="pokemon-item-link">
+            <div className={'container-item'} style={{ backgroundColor }}>
+                <div className={`image-div`}>
+                    <img className={'pokemon-img'} src={image} alt={name} />
+                    <img className={'pokeball-img'} src={pokeball} alt={"pokeball"} />
+                </div>
+                <div className="name-id-div">
+                    <p>#{id}</p>
+                    <p className={`name`}>{capitalizeFirstLetter(name)}</p>
+                    <div className={'type-div'}>
+                        {type.map((type, index) => (
+                            <p
+                                key={index}
+                                style={{ backgroundColor: getBackgroundColor(type) }}
+                                className={`type-p`}
+                            >
+                                {capitalizeFirstLetter(type)}
+                            </p>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
